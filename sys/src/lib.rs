34e5b0f626f107dfc8f4740681f67bc6bb4b27ca
eagle-fox <eaglefoxdev@gmail.com>
2024-07-28 18:41:38 +0200

    Update

--- sys/src/lib.rs
+++ sys/src/lib.rs
@@ -1,3 +1,22 @@
+//! This mainmodule provides Rust FFI bindings to the C header files `cencode.h` and `cdecode.h`
+//! from `libbase64`.
+//!
+//! The goal is to fully bind the C file to Rust, allowing us to use it in Rust
+//! just as we would in C. These are raw bindings and will likely require
+//! `unsafe` calls and possibly the `libc` crate to work with the data types.
+//!
+//! If you don't have `libc` available or prefer not to use it, you can find the
+//! correct type declarations in both `std::os::raw` and `core::ffi`.
+//! 
+//! `cencode.h` - c header for a base64 encoding algorithm
+//! `cdecode.h` - c header for a base64 decode algorithm
+//!
+//! This is part of the libb64 project, and has been placed in the public domain.
+//! For details, see [`lib64`](http://sourceforge.net/projects/libb64)
+//! 
+//! ```rust
+//! use libbase64_sys
+//! ```
 #![warn(missing_docs)]
 #![deny(missing_docs)]
 #![allow(
