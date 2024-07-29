10b6106f65ea5916ddab81299335ce2483377f22
eagle-fox <eaglefoxdev@gmail.com>
2024-07-29 17:35:54 +0200

    Update

--- src/error/kind.rs
+++ src/error/kind.rs
@@ -14,6 +14,50 @@ impl From<EncodeStatus> for EncodeKind {
     }
 }
 
+impl From<isize> for EncodeKind {
+    fn from(value: isize) -> Self {
+        let state = EncodeStatus::from(value);
+        EncodeKind { status: state.clone(), msg: Some(state.to_vec()) }
+    }
+}
+
+impl From<usize> for EncodeKind {
+    fn from(value: usize) -> Self {
+        let state = EncodeStatus::from(value);
+        EncodeKind { status: state.clone(), msg: Some(state.to_vec()) }
+    }
+}
+
+impl From<i16> for EncodeKind {
+    fn from(value: i16) -> Self {
+        let state = EncodeStatus::from(value);
+        EncodeKind { status: state.clone(), msg: Some(state.to_vec()) }
+    }
+}
+
+impl From<u16> for EncodeKind {
+    fn from(value: u16) -> Self {
+        let state = EncodeStatus::from(value);
+        EncodeKind { status: state.clone(), msg: Some(state.to_vec()) }
+    }
+}
+
+#[cfg(any(target_pointer_width = "32", target_pointer_width = "64"))]
+impl From<i32> for EncodeKind {
+    fn from(value: i32) -> Self {
+        let state = EncodeStatus::from(value);
+        EncodeKind { status: state.clone(), msg: Some(state.to_vec()) }
+    }
+}
+
+#[cfg(any(target_pointer_width = "32", target_pointer_width = "64"))]
+impl From<u32> for EncodeKind {
+    fn from(value: u32) -> Self {
+        let state = EncodeStatus::from(value);
+        EncodeKind { status: state.clone(), msg: Some(state.to_vec()) }
+    }
+}
+
 #[derive(Debug, Clone, Default)]
 pub struct DecodeKind {
     status: DecodeStatus,
