c2eaf93c44e8d64a65e5213bd1a77f4d32d09685
eagle-fox <eaglefoxdev@gmail.com>
2024-07-30 20:47:24 +0200

    Update

--- src/encode.rs
+++ src/encode.rs
@@ -1,3 +1,88 @@
-use crate::error::{EncodeError, EncodeKind, EncodeStatus};
-use crate::b64derive::{Send, Sync};
+//!
+#![allow(
+    unused,
+    unused_imports
+)]
+#![deny(missing_docs)]
+#![warn(missing_docs)]
 
+use super::libc;
+use super::std;
+use super::core;
+use super::b64derive::{Send, Sync};
+use super::b64sys::{base64_encode_block, base64_encode_blockend, base64_encode_value, base64_encodestate, base64_encodestep, base64_init_encodestate};
+
+///
+#[derive(Debug, Clone, Send, Sync)]
+pub struct EncodeState {
+    pub(crate) state: base64_encodestate,
+}
+
+impl From<base64_encodestate> for EncodeState {
+    fn from(value: base64_encodestate) -> Self {
+        EncodeState { state: value }
+    }
+}
+
+impl EncodeState {
+
+    ///
+    #[inline]
+    pub fn new() -> EncodeState {
+        let mut state_uninit = base64_encodestate { step: base64_encodestep::step_A, stepcount: 0, result: core::ptr::null_mut() };
+        unsafe { base64_init_encodestate(&mut state_uninit) };
+        EncodeState { state: state_uninit }
+    }
+}
+
+///
+#[derive(Debug, Clone, Send, Sync)]
+pub struct StateParser {
+
+    pub(crate) state: EncodeState,
+    pub(crate) parsed: Option<Vec<u8>>,
+    pub(crate) new: bool,
+
+}
+
+impl From<EncodeState> for StateParser {
+    fn from(value: EncodeState) -> Self {
+        StateParser { state: value, parsed: None, new: true }
+    }
+}
+
+impl From<base64_encodestate> for StateParser {
+    fn from(value: base64_encodestate) -> Self {
+        let state = EncodeState::from(value);
+        StateParser { state, parsed: None, new: true }
+    }
+}
+
+impl StateParser {
+    
+    ///
+    #[inline]
+    pub fn new() -> StateParser {
+        let state = EncodeState::new();
+        StateParser { state: state, parsed: None, new: true }
+    }
+
+    ///
+    #[inline]
+    pub const fn raw_parse(&self) -> base64_encodestate {
+        self.state.state
+    }
+
+    ///
+    #[inline]
+    pub const fn as_ptr(&self) -> *const base64_encodestate {
+        &self.state.state
+    }
+
+    ///
+    #[inline]
+    pub const fn as_mut_ptr(&mut self) -> *mut base64_encodestate {
+        &mut self.state.state
+    }
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,10 @@
-#![allow(
-    unused,
-    unused_imports
-)]
-
 #![feature(
-    deref_pure_trait,
-    vec_into_raw_parts
+    const_mut_refs
 )]
-
 extern crate libbase64_derive as b64derive;
 extern crate libbase64_sys as b64sys;
-extern crate libc;
 extern crate std;
 extern crate core;
+extern crate libc;
 
-pub(crate) mod error;
-pub(crate) mod encode;
+pub mod encode;
