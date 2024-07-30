88657624c47b6696877dcffea763183e1f4aa1af
eagle-fox <eaglefoxdev@gmail.com>
2024-07-30 13:18:31 +0200

    Update

--- src/error.rs
+++ src/error.rs
@@ -1,47 +1,251 @@
 pub(crate) mod code;
 pub(crate) mod kind;
 
-pub use code::{DecodeStatus, EncodeStatus};
-pub use kind::{DecodeKind, EncodeKind};
+use core::error::Error;
 
-#[derive(Debug, Clone, Default)]
+pub use crate::error::code::{DecodeStatus, EncodeStatus};
+pub use crate::error::kind::{DecodeKind, EncodeKind};
+
+#[derive(Debug, Clone)]
 pub struct EncodeError {
-    pub(crate) msg: Vec<u8>,
+    pub(crate) msg: Option<Vec<u8>>,
     pub(crate) kind: EncodeKind,
+}
+
+impl core::error::Error for EncodeError {
+    #[allow(deprecated)]
+    fn description(&self) -> &str {
+        if let Some(msg) = self.msg.clone() {
+            match ::core::str::from_utf8(&msg) {
+                Ok(msg) => Box::leak(msg.to_string().into_boxed_str()),
+                Err(_) => self.kind.description(),
+            }
+        } else {
+            self.kind.description()
+        }
+    }
+}
+
+impl core::fmt::Display for EncodeError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        if let Some(msg) = self.msg.clone() {
+            match ::core::str::from_utf8(&msg) {
+                Ok(msg) => f.write_str(msg),
+                Err(_) => self.kind.fmt(f),
+            }
+        } else {
+            self.kind.fmt(f)
+        }
+    }
 }
 
 impl EncodeError {
 
     #[inline]
-    pub fn new_custom(msg: &'static str, code: isize) -> EncodeError {
-        let msg = msg.as_bytes().to_vec();
+    pub fn new(msg: &'static str, code: isize) -> EncodeError {
         let kind = EncodeKind::from(code);
-        EncodeError { msg, kind }
+        EncodeError { msg: Some(msg.as_bytes().to_vec()), kind }
     }
 
     #[inline]
-    pub fn new_from_code(code: isize) -> EncodeError {
+    #[allow(deprecated)]
+    pub fn from_code(code: isize) -> EncodeError {
         let kind = EncodeKind::from(code);
-        let msg = kind.msg.clone().unwrap_or("Can't convert to base64".as_bytes().to_vec());
-        EncodeError { msg, kind }
+        let msg = kind.description();
+        EncodeError { msg: Some(msg.as_bytes().to_vec()), kind }
     }
 
     #[inline]
-    pub fn new_from_kind(kind: EncodeKind) -> EncodeError {
-        let msg = kind.msg.clone().unwrap_or("Can't convert to base64".as_bytes().to_vec());
-        EncodeError { msg, kind }
+    #[allow(deprecated)]
+    pub fn from_kind(kind: EncodeKind) -> EncodeError {
+        let msg = kind.description();
+        EncodeError { msg: Some(msg.as_bytes().to_vec()), kind }
     }
 
     #[inline]
-    pub fn new_from_status(status: EncodeStatus) -> EncodeError {
-        let kind = EncodeKind::from(status);
-        let msg = kind.msg.clone().unwrap_or("Can't convert to base64".as_bytes().to_vec());
-        EncodeError { msg, kind }
+    pub fn from_code_no_msg(code: isize) -> EncodeError {
+        let kind = EncodeKind::from(code);
+        EncodeError { msg: None, kind }
+    }
+
+    #[inline]
+    pub fn from_kind_no_msg(kind: EncodeKind) -> EncodeError {
+        EncodeError { msg: None, kind }
+    }
+
+    #[inline]
+    pub fn handle_print(&self) {
+        if let Some(msg) = self.msg.clone() {
+            match ::core::str::from_utf8(&msg) {
+                Ok(msg) => eprintln!("{}", msg),
+                Err(_) => self.kind.handle_print(),
+            }
+        } else {
+            self.kind.handle_print()
+        }
+    }
+
+    #[inline]
+    pub fn handle_panic(&self) -> ! {
+        if let Some(msg) = self.msg.clone() {
+            match ::core::str::from_utf8(&msg) {
+                Ok(msg) => panic!("{}", msg),
+                Err(_) => self.kind.handle_panic(),
+            }
+        } else {
+            self.kind.handle_panic()
+        }
+    }
+
+    #[inline]
+    pub fn handle_abort(&self) -> ! {
+        self.kind.handle_abort()
+    }
+
+    #[inline]
+    pub fn handle_exit(&self) -> ! {
+        self.kind.handle_exit()
+    }
+
+    #[inline]
+    pub fn abort_with_msg(&self) -> ! {
+        if self.msg.is_some() {
+            self.handle_print();
+            self.handle_abort();
+        } else {
+            self.kind.abort_with_msg();
+        }
+    }
+
+    #[inline]
+    pub fn exit_with_msg(&self) -> ! {
+        if self.msg.is_some() {
+            self.handle_print();
+            self.handle_exit();
+        } else {
+            self.kind.exit_with_msg();
+        }
     }
 }
 
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Clone)]
 pub struct DecodeError {
-    msg: Vec<u8>,
-    kind: DecodeKind,
+    pub(crate) msg: Option<Vec<u8>>,
+    pub(crate) kind: DecodeKind,
+}
+
+impl core::error::Error for DecodeError {
+    #[allow(deprecated)]
+    fn description(&self) -> &str {
+        if let Some(msg) = self.msg.clone() {
+            match ::core::str::from_utf8(&msg) {
+                Ok(msg) => Box::leak(msg.to_string().into_boxed_str()),
+                Err(_) => self.kind.description(),
+            }
+        } else {
+            self.kind.description()
+        }
+    }
+}
+
+impl core::fmt::Display for DecodeError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        if let Some(msg) = self.msg.clone() {
+            match ::core::str::from_utf8(&msg) {
+                Ok(msg) => f.write_str(msg),
+                Err(_) => self.kind.fmt(f),
+            }
+        } else {
+            self.kind.fmt(f)
+        }
+    }
+}
+
+impl DecodeError {
+
+    #[inline]
+    pub fn new(msg: &'static str, code: isize) -> DecodeError {
+        let kind = DecodeKind::from(code);
+        DecodeError { msg: Some(msg.as_bytes().to_vec()), kind }
+    }
+
+    #[inline]
+    #[allow(deprecated)]
+    pub fn from_code(code: isize) -> DecodeError {
+        let kind = DecodeKind::from(code);
+        let msg = kind.description();
+        DecodeError { msg: Some(msg.as_bytes().to_vec()), kind }
+    }
+
+    #[inline]
+    #[allow(deprecated)]
+    pub fn from_kind(kind: DecodeKind) -> DecodeError {
+        let msg = kind.description();
+        DecodeError { msg: Some(msg.as_bytes().to_vec()), kind }
+    }
+
+    #[inline]
+    pub fn from_code_no_msg(code: isize) -> DecodeError {
+        let kind = DecodeKind::from(code);
+        DecodeError { msg: None, kind }
+    }
+
+    #[inline]
+    pub fn from_kind_no_msg(kind: DecodeKind) -> DecodeError {
+        DecodeError { msg: None, kind }
+    }
+
+    #[inline]
+    pub fn handle_print(&self) {
+        if let Some(msg) = self.msg.clone() {
+            match ::core::str::from_utf8(&msg) {
+                Ok(msg) => eprintln!("{}", msg),
+                Err(_) => self.kind.handle_print(),
+            }
+        } else {
+            self.kind.handle_print()
+        }
+    }
+
+    #[inline]
+    pub fn handle_panic(&self) -> ! {
+        if let Some(msg) = self.msg.clone() {
+            match ::core::str::from_utf8(&msg) {
+                Ok(msg) => panic!("{}", msg),
+                Err(_) => self.kind.handle_panic(),
+            }
+        } else {
+            self.kind.handle_panic()
+        }
+    }
+
+    #[inline]
+    pub fn handle_abort(&self) -> ! {
+        self.kind.handle_abort()
+    }
+
+    #[inline]
+    pub fn handle_exit(&self) -> ! {
+        self.kind.handle_exit()
+    }
+
+    #[inline]
+    pub fn abort_with_msg(&self) -> ! {
+        if self.msg.is_some() {
+            self.handle_print();
+            self.handle_abort();
+        } else {
+            self.kind.abort_with_msg();
+        }
+    }
+
+    #[inline]
+    pub fn exit_with_msg(&self) -> ! {
+        if self.msg.is_some() {
+            self.handle_print();
+            self.handle_exit();
+        } else {
+            self.kind.exit_with_msg();
+        }
+    }
 }
